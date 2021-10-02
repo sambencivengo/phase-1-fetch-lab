ca8064fc4fe446cede1df66026f05b0f00c57dc3
Sam Bencivengo <sambencivengo@gmail.com>
2021-10-02 18:54:51 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
+const mainURL = 'https://anapioficeandfire.com/api/books';
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch(mainURL)
+    .then((resp) => resp.json())
+    .then((books) => renderBooks(books));
+  // .then((books) => forEach(books));
 }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
-  books.forEach(book => {
+  books.forEach((book) => {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   fetchBooks();
 });
